f8e41715d8a97866bc411e8c098054292c179d29
nikola-naydenov-hmcts <47384516+nikola-naydenov-hmcts@users.noreply.github.com>
2019-03-01 16:35:02 +0000

    Cache token from System Identity service (#16)

--- src/main/java/uk/gov/hmcts/reform/vault/credential/AccessTokenKeyVaultCredential.java
+++ src/main/java/uk/gov/hmcts/reform/vault/credential/AccessTokenKeyVaultCredential.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.vault.credential;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.credentials.AzureTokenCredentials;
 import org.apache.http.HttpResponse;
@@ -11,46 +14,66 @@ import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.HttpContext;
 
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class AccessTokenKeyVaultCredential extends AzureTokenCredentials {
 
     private static final String METADATA_HEADER = "Metadata";
 
-    private final String tokenEndpoint;
-
-    private final HttpClient httpClient;
+    private final LoadingCache<String, String> accessTokenCacheLoader;
 
     private static final TokenResponseHandler tokenResponseHandler = new TokenResponseHandler();
 
     public AccessTokenKeyVaultCredential(String tokenEndpoint, int maxRetries, int retryInterval) {
         super(AzureEnvironment.AZURE, null);
-        this.tokenEndpoint = tokenEndpoint;
-        this.httpClient = HttpClientBuilder.create().setServiceUnavailableRetryStrategy(
-            new ServiceUnavailableRetryStrategy() {
-                @Override
-                public boolean retryRequest(final HttpResponse response,
-                                            final int executionCount, final HttpContext context) {
-                    int statusCode = response.getStatusLine().getStatusCode();
-                    return statusCode >= HttpStatus.SC_INTERNAL_SERVER_ERROR
-                        && executionCount < maxRetries;
-                }
-
-                @Override
-                public long getRetryInterval() {
-                    return retryInterval;
-                }
-            }).build();
+        accessTokenCacheLoader = CacheBuilder.newBuilder()
+            .expireAfterWrite(1, TimeUnit.HOURS)
+            .build(new AccessTokenKeyVaultCredential
+                .AccessTokenCacheLoader(tokenEndpoint, maxRetries, retryInterval));
     }
 
     @Override
-    public String getToken(String resource) throws IOException {
+    public String getToken(String resource) {
+        return accessTokenCacheLoader.getUnchecked(resource);
+    }
 
-        HttpUriRequest request = RequestBuilder.get()
+    static final class AccessTokenCacheLoader extends CacheLoader<String, String> {
+
+        private final HttpClient httpClient;
+
+        private final HttpUriRequest request;
+
+        AccessTokenCacheLoader(String tokenEndpoint, int maxRetries, int retryInterval) {
+            this.request = RequestBuilder.get()
                 .setUri(tokenEndpoint)
                 .setHeader(METADATA_HEADER, Boolean.TRUE.toString())
                 .build();
 
-        return httpClient.execute(request, tokenResponseHandler);
+            this.httpClient = HttpClientBuilder.create().setServiceUnavailableRetryStrategy(
+                new ServiceUnavailableRetryStrategy() {
+                    @Override
+                    public boolean retryRequest(final HttpResponse response,
+                                                final int executionCount, final HttpContext context) {
+                        int statusCode = response.getStatusLine().getStatusCode();
+                        return statusCode >= HttpStatus.SC_INTERNAL_SERVER_ERROR
+                            && executionCount < maxRetries;
+                    }
+
+                    @Override
+                    public long getRetryInterval() {
+                        return retryInterval;
+                    }
+                }).build();
+
+        }
+
+        @Override
+        public String load(String resource) throws Exception {
+            String key = httpClient.execute(request, tokenResponseHandler);
+            if (key == null) {
+                throw new NullPointerException();
+            }
+            return key;
+        }
     }
 }
