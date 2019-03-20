808160c024dc4e0ae7c49ff6d6f7414779d6efe8
Nikola Naydenov <nikolanaydenov@AML0089.local>
2019-03-20 10:26:51 +0000

    SIDM-2184 Indentation must look stupid according to travis ci

--- src/main/java/uk/gov/hmcts/reform/security/keyvault/KeyVaultService.java
+++ src/main/java/uk/gov/hmcts/reform/security/keyvault/KeyVaultService.java
@@ -61,13 +61,13 @@ final class KeyVaultService {
 
     KeyVaultService(KeyVaultConfig keyVaultConfig) {
         this(keyVaultConfig, ((KeyVaultClientProvider) keyVaultConfig1 -> {
-            if (StringUtils.isNoneEmpty(keyVaultConfig1.getVaultClientId(), keyVaultConfig1.getVaultClientKey())) {
-                return new KeyVaultClient(new ClientSecretKeyVaultCredential(keyVaultConfig1.getVaultClientId(),
-                    keyVaultConfig1.getVaultClientKey()));
-            } else if (StringUtils.isNotEmpty(keyVaultConfig1.getVaultMsiUrl())) {
-                return new KeyVaultClient(new AccessTokenKeyVaultCredential(keyVaultConfig1.getVaultMsiUrl(),
-                    keyVaultConfig1.getVaultErrorMaxRetries(), keyVaultConfig1.getVaultErrorRetryIntervalMillis()));
-            }
+                    if (StringUtils.isNoneEmpty(keyVaultConfig1.getVaultClientId(), keyVaultConfig1.getVaultClientKey())) {
+                        return new KeyVaultClient(new ClientSecretKeyVaultCredential(keyVaultConfig1.getVaultClientId(),
+                            keyVaultConfig1.getVaultClientKey()));
+                    } else if (StringUtils.isNotEmpty(keyVaultConfig1.getVaultMsiUrl())) {
+                        return new KeyVaultClient(new AccessTokenKeyVaultCredential(keyVaultConfig1.getVaultMsiUrl(),
+                            keyVaultConfig1.getVaultErrorMaxRetries(), keyVaultConfig1.getVaultErrorRetryIntervalMillis()));
+                    }
             throw new IllegalArgumentException("System properties do not define which KeyVaultClient to create");
         }).getClient(keyVaultConfig));
     }
