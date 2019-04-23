8e7316eb011b89a2098479481a3581fb54141d07
Nikola Naydenov <nikolanaydenov@AML0089.local>
2019-04-23 10:03:48 +0100

    SIDM-V2

--- src/test/java/uk/gov/hmcts/reform/security/keyvault/KeyVaultServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/security/keyvault/KeyVaultServiceTest.java
@@ -11,7 +11,6 @@ import com.microsoft.azure.keyvault.models.SecretItem;
 import com.microsoft.azure.keyvault.requests.SetSecretRequest;
 import com.microsoft.azure.keyvault.webkey.JsonWebKeySignatureAlgorithm;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
